2135d7c86b676bd6302ec20d669d93756f68919e
nicolas <nicolasmahecha1125@gmail.com>
2023-10-11 21:51:36 -0500

    ii

--- src/main/java/controladorServicio/ArchiveController.java
+++ src/main/java/controladorServicio/ArchiveController.java
@@ -2,21 +2,21 @@ package controladorServicio;
 
 import ServicioProcesamiento.ServiceProcessCsv;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
+@RequestMapping("/service")
 public class ArchiveController {
 
     @Autowired
     private ServiceProcessCsv csvService;
 
-    @GetMapping("/process-csv")
+    @PostMapping("/process-csv")
     public List<String[]> processCsv(@RequestParam String fileName) {
         csvService.procesarCsv("people.cvs");
+        csvService.imprimirLinea();
         return csvService.getCsvData();
     }
 }
